7f746bb1ccc3903346f86db3b62b429aaf38b0ef
Mike Pettman <mp_palmatum@yahoo.com>
2017-10-11 16:47:38 -0500

    can push employees into companies array

--- modules/companies.module.js
+++ modules/companies.module.js
@@ -1,21 +1,15 @@
-// const router = require('express').Router;
-
-class Company {
-    constructor(name){
-        this.name = name;
-        this.employees = [];
+module.exports =
+    class Company {
+        constructor(name) {
+            this.name = name;
+            this.employees = [];
+        }
+        addEmployee(emp) {
+            // push emloyee into this.employees array
+            console.log('push employee func');
+            this.employees.push(emp);
+        }
+        addBonus() {
+            console.log('addBonus func');
+        }
     }
-    addEmployee(emp) {
-        // push emloyee into this.employees array
-        console.log('push employee func');
-        
-    }
-    addBonus() {
-        console.log('addBonus func');
-    }
-}
-
-let Microsoft = new Company('Microsoft');
-
-
-module.exports = Company();
--- server.js
+++ server.js
@@ -3,16 +3,22 @@ const app = express();
 const port = 3000;
 const bodyParser = require('body-parser');
 const employees = require('./modules/employees.module');
-// const companies = require('./modules/companies.module');
-
+const companies = require('./modules/companies.module');
 
 // app.listen(port, function(){
 //     console.log('Listening on port', port);
 // });
 
-
 app.use(bodyParser.json());
 
-let Joey = new employees('Joey');
-
+// testing employees.modules.js below
+let Joey = new employees('Joey', 'May114', 80000, 5);
 console.log(Joey);
+
+// testing companies.module.js below
+let Microsoft = new companies('Microsoft');
+console.log(Microsoft);
+
+// testing connection between employees and companies
+Microsoft.addEmployee(Joey);
+console.log(Microsoft);
