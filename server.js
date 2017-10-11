c8e85289ff15641e13a1517167e162f37172be8c
Mike Pettman <mp_palmatum@yahoo.com>
2017-10-11 16:15:03 -0500

    class structures added

--- server.js
+++ server.js
@@ -1,8 +1,44 @@
-var express = require('express');
-var app = express();
-var port = 3000;
+const express = require('express');
+const app = express();
+const port = 3000;
+const bodyParser = require('body-parser');
 
 
 app.listen(port, function(){
     console.log('Listening on port', port);
 });
+
+app.use(bodyParser.json());
+
+class Employee {
+    constructor(name, startDate, salary, reviewRating){
+        this.name = name;
+        this.startDate = startDate;
+        this.salary = salary;
+        this.reviewRating = reviewRating;
+    }
+    calcDays() {
+        // calcDays code
+        console.log('calcDays func called');
+    }
+}
+
+let Joey = new Employee('Joey', '10-3-2017', 80000, 5);
+
+
+class Company {
+    constructor(name){
+        this.name = name;
+        this.employees = [];
+    }
+    addEmployee(emp) {
+        // push emloyee into this.employees array
+        console.log('push employee func');
+        
+    }
+    addBonus() {
+        console.log('addBonus func');
+    }
+}
+
+let Microsoft = new Company('Microsoft');
